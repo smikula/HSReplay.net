1c2ae3714a376c1adee3462c265a66c4d039799f
Jerome Leclanche <jerome@leclan.ch>
2016-07-18 22:44:33 +0300

    Allow runserver without Whitenoise

--- wsgi.py
+++ wsgi.py
@@ -10,11 +10,15 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hsreplaynet.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
+try:
+	from whitenoise.django import DjangoWhiteNoise
+	application = DjangoWhiteNoise(application)
+except ImportError as e:
+	sys.stderr.write("Not running with Whitenoise (%s)\n" % (e))
