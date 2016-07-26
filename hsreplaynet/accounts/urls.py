55c57122dffe31302891b14683b75f5f874ceda9
Jerome Leclanche <jerome@leclan.ch>
2016-07-26 18:26:45 +0300

    Implement account deletion view
    When an account deletion is requested, it is stored in the user model
    to be deleted in batch later.
    TODO: Immediately change visibility of owned replays if replay deletion
    is requested as well.

--- hsreplaynet/accounts/urls.py
+++ hsreplaynet/accounts/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from .views import ClaimAccountView
+from .views import ClaimAccountView, DeleteAccountView
 
 
 urlpatterns = [
 	url(r"^$", TemplateView.as_view(template_name="account/edit.html"), name="account_edit"),
 	url(r"^claim/(?P<id>[\w-]+)/$", ClaimAccountView.as_view(), name="account_claim"),
-	url(r"^delete/$", TemplateView.as_view(template_name="account/delete.html"), name="account_delete"),
+	url(r"^delete/$", DeleteAccountView.as_view(), name="account_delete"),
 ]
--- hsreplaynet/accounts/views.py
+++ hsreplaynet/accounts/views.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.contrib import messages
+from django.contrib.auth import logout
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
-from django.views.generic import View
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.utils.timezone import now
+from django.views.generic import TemplateView, View
 from hsreplaynet.utils import get_uuid_object_or_404
 from .models import AccountClaim
 
@@ -17,4 +20,20 @@ class ClaimAccountView(LoginRequiredMixin, View):
 		msg = "You have claimed your account. Yay!"
 		# XXX: using WARNING as a hack to ignore login/logout messages for now
 		messages.add_message(request, messages.WARNING, msg)
-		return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+		return redirect(settings.LOGIN_REDIRECT_URL)
+
+
+class DeleteAccountView(LoginRequiredMixin, TemplateView):
+	template_name = "account/delete.html"
+
+	def post(self, request):
+		if not request.POST.get("delete_confirm"):
+			return redirect("account_delete")
+		user = request.user
+		user.is_active = False
+		user.delete_account_request = now()
+		if request.POST.get("delete_replays"):
+			user.delete_replay_data = True
+		user.save()
+		logout(self.request)
+		return redirect(reverse("home"))
