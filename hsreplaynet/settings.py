c342f03eb2d1ab01bddc9f621efe0760c4ed928e
Jerome Leclanche <jerome@leclan.ch>
2016-07-28 03:20:02 +0300

    Use ManifestStaticFilesStorage as our default STATICFILES_STORAGE

--- hsreplaynet/settings.py
+++ hsreplaynet/settings.py
@@ -127,6 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, "hsreplaynet", "static"),
 ]
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 WEBPACK_LOADER = {
 	"DEFAULT": {
