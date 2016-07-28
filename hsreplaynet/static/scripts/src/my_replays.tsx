838079345754057281019f59cb877d8c1c240bb6
Jerome Leclanche <jerome@leclan.ch>
2016-07-28 18:46:08 +0300

    Rewrite replay listing in React

--- hsreplaynet/static/scripts/src/my_replays.tsx
+++ hsreplaynet/static/scripts/src/my_replays.tsx
@@ -1,3 +1,18 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
+import GameHistoryList from "./components/GameHistory";
 
-console.log("My replays, with React", React.version);
+
+function renderReplayListing() {
+	let r = $.getJSON("/api/v1/games", {username: $("body").data("username")}, function(data) {
+		console.log(data)
+		if (data.count) {
+			ReactDOM.render(
+				<GameHistoryList games={data["results"]}/>,
+				document.getElementById("my-games-container")
+			);
+		}
+	});
+}
+
+renderReplayListing();
