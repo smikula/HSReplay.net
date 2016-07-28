c6324ede4b3963568351591c2f50ff23124aaa11
Jerome Leclanche <jerome@leclan.ch>
2016-07-28 05:08:02 +0300

    Do not 404 on missing UploadEvent, maybe it's still uploading

--- hsreplaynet/uploads/views.py
+++ hsreplaynet/uploads/views.py
@@ -1,13 +1,18 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.views.generic import View
 from .models import UploadEvent
 
 
 class UploadDetailView(View):
 	def get(self, request, shortid):
-		upload = get_object_or_404(UploadEvent, shortid=shortid)
-		if upload.game:
-			return HttpResponseRedirect(upload.game.get_absolute_url())
+		try:
+			upload = UploadEvent.objects.get(shortid=shortid)
+		except UploadEvent.DoesNotExist:
+			# It is possible the UploadEvent hasn't been created yet.
+			upload = None
+		else:
+			if upload.game:
+				return HttpResponseRedirect(upload.game.get_absolute_url())
 
 		return render(request, "uploads/processing.html", {"upload": upload})
